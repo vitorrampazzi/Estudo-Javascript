ac31000e1d1da52f5256ae3bda67e5f7cb75ab5b
vitorrampazzi <vitor.rampazzi@gmail.com>
2025-04-29 14:50:05 -0300

    ex Objetos

---
+++ 3. Objetos/script.js
@@ -0,0 +1,11 @@
+titulo = true
+autor = false
+ano = false
+lido = true
+genero = 'obscuro'
+
+livro = {'titulo': titulo, 'autor': autor, 'ano': ano, 'lido': lido, 'genero': genero}
+
+console.log(livro.titulo)
+console.log(livro['titulo'])
+console.log(livro['genero'])
