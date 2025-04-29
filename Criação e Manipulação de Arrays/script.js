93edf814c2053131ababfcafe82f1931bf659909
vitorrampazzi <vitor.rampazzi@gmail.com>
2025-04-29 14:32:12 -0300

    exercicio 1 e 2

---
+++ Criação e Manipulação de Arrays/script.js
@@ -0,0 +1,6 @@
+const filmes = ['planeta dos macacos', 'como treinar seu dragao', 'dracula', 'um minuto para o fim do mundo']
+
+filmes.shift()
+filmes.push('interestelar')
+
+console.table(filmes)
---
+++ Métodos de Array/script.js
@@ -0,0 +1,7 @@
+const frutas = ["Maçã", "Banana", "Laranja", "Uva"];
+
+const mostrarFrutasEmCaixaAlta = (fruta) => {
+    console.log(fruta.toUpperCase())
+}
+
+frutas.forEach(mostrarFrutasEmCaixaAlta)
