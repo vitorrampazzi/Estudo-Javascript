4e4de6c50e13dd759458bd856c1e74d7f3aded76
vitorrampazzi <vitor.rampazzi@gmail.com>
2025-05-19 00:05:20 -0300

    adicionando a Manipulação de Arrays

--- 1. Criação e Manipulação de Arrays/script.js
+++ 1. Criação e Manipulação de Arrays/script.js
@@ -1,6 +1,30 @@
-const filmes = ['planeta dos macacos', 'como treinar seu dragao', 'dracula', 'um minuto para o fim do mundo']
+// const filmes = ['planeta dos macacos', 'como treinar seu dragao', 'dracula', 'um minuto para o fim do mundo']
+// filmes.shift()
+// filmes.push('interestelar')
+// console.table(filmes)
 
-filmes.shift()
-filmes.push('interestelar')
+// const tarefas = ['jogar', 'limpar', 'dançar']
+// for(let tarefa of tarefas){
+//     console.log(tarefa)
+// }
 
-console.table(filmes)
+// const calculadora = {
+//     soma(a, b){
+//         return (a + b)
+//     },
+//     subtracao(a, b){
+//         return (a - b)
+//     },
+//     multiplicacao(a, b){
+//         return (a * b)
+//     },
+//     divisao(a, b){
+//         if (b === 0) return "Erro: Divisão por zero";
+//         return (a / b)
+//     }
+// }
+// console.log("Soma:", calculadora.soma(10, 5));
+// console.log("Subtração:", calculadora.subtracao(10, 5));
+// console.log("Multiplicação:", calculadora.multiplicacao(10, 5));
+// console.log("Divisão:", calculadora.divisao(10, 5));
+
